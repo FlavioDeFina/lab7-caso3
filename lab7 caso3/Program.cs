df42cba698e9a3064c888f5fa465baca3dbea277
José Manuel <Jos Manuel@DESKTOP-L67G7N4>
2023-10-04 23:24:50 -0500

    Agregar archivos de proyecto.

---
+++ lab7 caso3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7_caso3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el final de los años: ");
+            int años = int.Parse(Console.ReadLine());
+            int inicio = 1900;
+
+            for (int i = inicio;i <= años; i++) 
+            {
+                if ((i %4==0 && i%100 !=0)||(i %400 == 0)) 
+                {
+                    Console.WriteLine("aqui hay un años bisiesto: "+i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
